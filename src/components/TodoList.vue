924a1bf171d6e6d789c81b6e077c0f1de9c18bd9
liqili <liqili2005@163.com>
2020-01-09 21:10:21 +1300

    initial commit

---
+++ src/components/TodoList.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="hello">
+    <h1>{{ title }}</h1>
+    <ul>
+      <li
+        v-for="item in todoItems"
+        :key="item.id"
+        :class="item.completed ? 'checked':''"
+      >{{ item.title }},{{ item.completed ? 'Complete':'Not Completed' }}</li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class TodoList extends Vue {
+  @Prop() private title!: string;
+  @Prop({ default: [] }) private todoItems!: string[];
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+/* Include the padding and border in an element's total width and height */
+* {
+  box-sizing: border-box;
+}
+
+/* Remove margins and padding from the list */
+ul {
+  margin: 0;
+  padding: 0;
+  text-align: left;
+}
+
+/* Style the list items */
+ul li {
+  cursor: pointer;
+  position: relative;
+  padding: 12px 8px 12px 40px;
+  background: #eee;
+  font-size: 18px;
+  transition: 0.2s;
+
+  /* make the list items unselectable */
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+
+/* Set all odd list items to a different color (zebra-stripes) */
+ul li:nth-child(odd) {
+  background: #f9f9f9;
+}
+
+/* Darker background-color on hover */
+ul li:hover {
+  background: #ddd;
+}
+
+/* When clicked on, add a background color and strike out text */
+ul li.checked {
+  background: #42b983;
+  color: #fff;
+  text-decoration: line-through;
+}
+
+/* Add a "checked" mark when clicked on */
+ul li.checked::before {
+  content: "";
+  position: absolute;
+  border-color: #fff;
+  border-style: solid;
+  border-width: 0 2px 2px 0;
+  top: 10px;
+  left: 16px;
+  transform: rotate(45deg);
+  height: 15px;
+  width: 7px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <TodoList title="Your Todo List" v-bind:todoItems="todoItems" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import TodoList from '@/components/TodoList.vue';
+
+export default {
+  name: 'home',
+  components: {
+    TodoList
+  },
+  data () {
+    return {
+      todoItems: [
+        {
+          userId: 1,
+          id: 1,
+          title: 'delectus aut autem',
+          completed: false
+        },
+        {
+          userId: 1,
+          id: 2,
+          title: 'quis ut nam facilis et officia qui',
+          completed: false
+        },
+        {
+          userId: 1,
+          id: 3,
+          title: 'fugiat veniam minus',
+          completed: false
+        }
+      ]
+    };
+  },
+  mounted () {
+    fetch('https://jsonplaceholder.typicode.com/todos/')
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error('Error occurred...');
+        }
+      })
+      .then(responseJson => {
+        this.todoItems = responseJson;
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+};
+</script>
